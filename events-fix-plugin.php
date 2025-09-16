9d057b6855a6a116a23a557591159cfd9adcca38
Patrick Robrecht <mail@patrick-robrecht.de>
2025-09-16 22:40:21 +0200

    Adjust code for compliance with Events Manager 7.1+, release changes as version 0.3

--- events-fix-plugin.php
+++ events-fix-plugin.php
@@ -1,19 +1,19 @@
-<?php 
-/*
-Plugin Name: Gutenberg fix for Events manager
-Plugin URI: 
-Description: Gutenberg fix for Events manager
-Author: audun
-Version: 0.2
-Author URI: 
-License: GPL2
-*/
+<?php
+/**
+ * Plugin Name: Events Manager - Gutenberg fix
+ * Plugin URI: https://github.com/audunmb/wp-events-plugin-gutenberg-fix
+ * Description: Fix display of event categories and tags when using the Gutenberg editor to edit events in the Events Manager plugin.
+ * Author: audun
+ * Version: 0.3
+ * License: GPLv3
+ * License URI: https://www.gnu.org/licenses/gpl-3.0.html
+ */
 
-function bsos_gutenberg_categories_fix($args) {
-	$args['show_in_rest'] = true;
-	return $args;
+function bsos_gutenberg_categories_fix($args)
+{
+    $args['show_in_rest'] = true;
+    return $args;
 }
 
-
-add_filter('em_ct_categories','bsos_gutenberg_categories_fix');
-add_filter('em_ct_tags','bsos_gutenberg_categories_fix');
+add_filter('em_cpt_categories', 'bsos_gutenberg_categories_fix');
+add_filter('em_cpt_tags', 'bsos_gutenberg_categories_fix');
